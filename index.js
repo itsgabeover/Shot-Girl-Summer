744e93de1dc5c18b06c06871670e5711db998e4f
Gabriel Miranda <gabe.enrique.miranda@gmail.com>
2021-08-25 15:33:42 -0700

    add event listener for playlist table

--- index.js
+++ index.js
@@ -1,57 +1,72 @@
+fetch('http://localhost:3000/songs')
+     .then(res => res.json())
+     .then(songObjects => {
+         songObjects.forEach(song => {
+           renderSongList(song)
+         })
+        })
+function renderSongList(song){
+    const songTable = document.getElementById('table-body')
+    const row = document.createElement('tr')
+    songTable.append(row)
 
-const createPlaylist = () => {
+    const songName = document.createElement('td')
+    songName.innerText = song.name
+    row.append(songName)
+    
+    const artistName = document.createElement('td')
+    artistName.innerText = song.artist
+    row.append(artistName)
+
+    const addToPlaylist = document.createElement('button')
+    addToPlaylist.innerText = "ADD"
+    row.append(addToPlaylist)
+    
 }
 
-    // fetch('https://accounts.spotify.com/authorize')
-    // .then(res => res.json())
-    // .then(musicList => {
-    //     musicList.forEach(oneSong => {
+function createPlaylistTable() {
 
+}
 
-    //     })
-    // })
+const playlistForm = document.getElementById('create-playlist')
 
+playlistForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const playlistName = document.querySelector('#playlist-name')
+    const userInput = e.target['playlist'].value
+    console.log(userInput)
 
-    // function generate_table() {
-    //   // get the reference for the body
-    //   var body = document.getElementsByTagName("body")[0];
-    
-    //   // creates a <table> element and a <tbody> element
-    //   var tbl = document.createElement("table");
-    //   var tblBody = document.createElement("tbody");
-    
-    //   // creating all cells
-    //   for (var i = 0; i < 2; i++) {
-    //     // creates a table row
-    //     var row = document.createElement("tr");
-    
-    //     for (var j = 0; j < 2; j++) {
-    //       // Create a <td> element and a text node, make the text
-    //       // node the contents of the <td>, and put the <td> at
-    //       // the end of the table row
-    //       var cell = document.createElement("td");
-    //       var cellText = document.createTextNode("cell in row "+i+", column "+j);
-    //       cell.appendChild(cellText);
-    //       row.appendChild(cell);
-    //     }
-    
-    //     // add the row to the end of the table body
-    //     tblBody.appendChild(row);
-    //   }
-    
-    //   // put the <tbody> in the <table>
-    //   tbl.appendChild(tblBody);
-    //   // appends <table> into <body>
-    //   body.appendChild(tbl);
-    //   // sets the border attribute of tbl to 2;
-    //   tbl.setAttribute("border", "2");
-    // }
+    playlistName.textContent = `Playlist Name: ${userInput}`
 
- 
+})
 
 
 
-function deleteSongFromPlayList(){
+//         const dogForm = document.getElementById(‘dog-form’)
+//         data.forEach( (dog) => {
+//             const table = document.getElementById(‘table-body’)
+//             const row = document.createElement(‘tr’)
+//             table.append(row)
+//             const dogNames = document.createElement(‘td’)
+//             dogNames.innerText = dog.name
+//             row.append(dogNames)
+//             const dogBreed = document.createElement(‘td’)
+//             dogBreed.innerText = dog.breed
+//             row.append(dogBreed)
+//             const dogSex = document.createElement(‘td’)
+//             dogSex.innerText = dog.sex
+//             row.append(dogSex)
+//             const dogButton = document.createElement(‘button’)
+//             dogButton.innerText = ‘Edit Dog’
+//             row.append(dogButton)
+//             dogButton.addEventListener(‘click’, (e) => {
+//                 e.preventDefault()
+//                 console.log(dogForm.name, dog.name)
+//                 dogForm.name.value = dog.name
+//                 dogForm.breed.value = dog.breed
+//                 dogForm.sex.value = dog.sex
+
+function deleteSongFromPlayList(){ 
     //
     let li = document.createElement('li')
     let button = document.createElement('button')
